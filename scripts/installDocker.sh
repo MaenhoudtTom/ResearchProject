b2ece33067f9c6c1662e6b63bd856b085d1116cb
MaenhoudtTom <tom.maenhoudt@student.howest.be>
2022-03-30 10:42:15 +0200

    Update installDocker
    Changed installation to the convencience script

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -23,13 +23,8 @@ fi
 echo "----------------------------------------------"
 echo "Downloading and installing Docker"
 echo "----------------------------------------------"
-apt install ca-certificates curl gnupg lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-apt install docker-ce docker-ce-cli containerd.io -y
-systemctl enable docker.service
-systemctl enable containerd.service
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 usermod -aG docker $1
 echo "----------------------------------------------"
 echo "Finished installing Docker"
